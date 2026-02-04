822a43480c272a18ad47d5130ae4c0f363aab3b7
staketokens <159169652+staketokens@users.noreply.github.com>
2026-02-04 13:36:27 +0100

    changed

--- web/app/sitemap.ts
+++ web/app/sitemap.ts
@@ -36,6 +36,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/free-invoice-generator`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/sales-tracking-ledger`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
